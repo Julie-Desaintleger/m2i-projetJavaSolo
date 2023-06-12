149c553f1aa87eee393b82b636ae5ecae0baecd4
Julie Desaintléger <julie.desaintleger@gmail.com>
2023-06-12 11:51:09 +0200

    modif_11h50

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -4,7 +4,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Bonjour le monde ; lundi 16 juin 2023");
+		System.out.println("Bonjour le monde ; lundi 16 juin 2023 11h50");
 	}
 
 }
