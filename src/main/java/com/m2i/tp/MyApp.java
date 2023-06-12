f52e5dbc50411ea628c62f443e43ddb0b73f84a6
Julie Desaintléger <julie.desaintleger@gmail.com>
2023-06-12 12:26:36 +0200

    Ajout Readme et modif MyApp

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -5,6 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bonjour le monde ; lundi 16 juin 2023 12h15");
+		System.out.println("modif a 12h26");
 	}
 
 }
