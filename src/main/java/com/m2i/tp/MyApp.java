a712a6509a12d03f81490ae2629114583d51a89b
Julie Desaintléger <julie.desaintleger@gmail.com>
2023-06-12 12:28:08 +0200

    test commit&push bidon

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -5,7 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bonjour le monde ; lundi 16 juin 2023 12h15");
-		System.out.println("modif a 12h26");
+		System.out.println("modidsffsf a 12h26");
 	}
 
 }
