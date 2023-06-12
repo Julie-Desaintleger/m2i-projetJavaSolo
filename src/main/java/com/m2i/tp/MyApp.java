d088edd7351684d03478a931d045f22fc491f5c7
Julie Desaintléger <julie.desaintleger@gmail.com>
2023-06-12 13:52:41 +0200

    devA the best

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -6,6 +6,7 @@ public class MyApp {
 		// TODO Auto-generated method stub
 		System.out.println("Bonjour le monde ; lundi 16 juin 2023 12h15");
 		System.out.println("modidsffsf a 12h26");
+		System.out.println("devA is the best");
 	}
 
 }
